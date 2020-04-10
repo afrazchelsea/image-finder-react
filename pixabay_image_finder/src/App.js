b52fb3583a183e2d03b06870cb9fc290cbc5c129
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-04-10 07:16:09 +0530

    first

---
+++ pixabay_image_finder/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import NavBar from "./Components/navbar/NavBar";
+import Search from "./Components/search/Search";
+
+class App extends React.Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
