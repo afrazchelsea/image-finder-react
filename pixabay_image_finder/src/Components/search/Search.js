564bf46a269cda900577e89a4fb4298e32e5fc00
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-05-02 08:34:53 +0530

    completed

--- pixabay_image_finder/src/Components/search/Search.js
+++ pixabay_image_finder/src/Components/search/Search.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -9,10 +11,30 @@ class Search extends Component {
     amount: 15,
     apiUrl: "https://pixabay.com/api",
     apiKey: "15956456-9d780ed4f55db637895cafc47",
-    image: [],
+    images: [],
   };
 
+  onTextChange = (e) => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      if (val === "") {
+        this.setState({
+          images: [],
+        });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then((res) => this.setState({ images: res.data.hits }))
+          .catch((err) => console.log(err));
+      }
+    });
+  };
+
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -36,6 +58,9 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
