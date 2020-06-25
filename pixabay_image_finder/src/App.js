b6ac210b9f45445489d62ceaaf3db21a5e2f52ac
Afraz Momin <41325430+afrazchelsea@users.noreply.github.com>
2020-06-25 20:53:23 +0530

    update

--- pixabay_image_finder/src/App.js
+++ pixabay_image_finder/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./Components/navbar/NavBar";
 import Search from "./Components/search/Search";
